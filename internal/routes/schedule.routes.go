075b92e67e59a0f74225a8e33e262bfe359b7964
Imanul Aufa <imanulaufa@gmail.com>
2025-06-06 14:00:22 +0700

    feat(schedule route): create schedule route and add get schedule by id route

---
+++ internal/routes/schedule.routes.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
+	"minitask1.go/internal/handlers"
+	"minitask1.go/internal/middleware"
+	"minitask1.go/internal/repositories"
+)
+
+func AddScheduleRoutes(router *gin.Engine, db *pgxpool.Pool, rdb *redis.Client, mdw *middleware.Middleware) {
+	scheduleRepo := repositories.NewScheduleRepository(db, rdb)
+	scheduleHandler := handlers.NewScheduleHandler(scheduleRepo)
+
+	router.GET("/tickets/:id", scheduleHandler.GetScheduleHandler)
+}
